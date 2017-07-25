20a94369c387fd29de47a764ede3f25b77c13eb0
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-07-25 18:07:31 +0200

    index page OK

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .models import Album
 
 def index(request):
-    albums = Album.objects.order_by('-created_at')[:5]
+    albums = Album.objects.order_by('-created_at')[:12]
     context = {
         'albums': albums,
     }
