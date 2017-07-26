3f392173c33f8758d4d80099c101f93d7337658f
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-07-26 12:46:26 +0200

    detail page done

--- disquaire_project/store/urls.py
+++ disquaire_project/store/urls.py
@@ -6,5 +6,5 @@ app_name = 'store'
 
 
 urlpatterns = [
-    # url(r'^$', views.index, name='index'),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,11 +1,23 @@
-# from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Album
 
+
+
 def index(request):
-    albums = Album.objects.order_by('-created_at')[:12]
+    albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
     context = {
         'albums': albums,
     }
     return render(request, 'store/index.html', context)
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    artists = [artist.name for artist in album.artists.all()]
+    artists_name = " ".join(artists)
+    context = {
+        'album_title': album.title,
+        'artists_name': artists_name
+    }
+    return render(request, 'store/detail.html', context)
